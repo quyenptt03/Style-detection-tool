667fa8b982baba794901e47b4e70a72f79e6009d
quyenptt03 <quyenptt03@gmail.com>
2025-04-25 15:52:03 +0700

    fix publish api

--- StyleDetectionTool/Program.cs
+++ StyleDetectionTool/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.Playwright;
 using StyleDetectionTool.Models;
 using StyleDetectionTool.Services;
+using System.Diagnostics;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,11 +26,26 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5999);
+});
+
 builder.Services.AddScoped<ThemeConfig>();
 builder.Services.AddScoped<StyleCheckingService>();
 builder.Services.AddHttpClient<ApiService>();
 
 var app = builder.Build();
+
+
+int exitCode = Microsoft.Playwright.Program.Main(new[] { "install", "chromium" });
+if (exitCode != 0)
+{
+    throw new Exception($"Failed to install Playwright with exit code {exitCode}.");
+}
+
+// Initialize Playwright
+var playwright = await Playwright.CreateAsync();
 
 app.UseCors("AllowAll");
 app.UseCors("AllowAngularDev");
